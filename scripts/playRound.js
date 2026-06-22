8d9c93ffda5c43e86abfc07ade9c351987ab6007
GNevesRPereira <92853292+GNevesRPereira@users.noreply.github.com>
2026-06-22 10:42:52 +0100

    Updates

--- scripts/playRound.js
+++ scripts/playRound.js
@@ -1,18 +1,11 @@
 
 const computerSelection = computerRandomGameValue;
-const playerSelection = "";
+const playerSelection = userPlayValidate();
 
 function playRound(computerSelection, playerSelection) {
-
+    console.log(computerSelection);
+    console.log(playerSelection);
+    ;
 }
 
-console.log(computerSelection);
 
-
-
-function userPlayValidate() {
-    let userPlay = prompt("Your turn human! Now, play!!!"," ");
-    if (userPlay != null){
-    console.log(userPlay);
-    }
-}
---
+++ scripts/userPlayValidate.js
@@ -0,0 +1,6 @@
+function userPlayValidate() {
+    let userPlay = prompt("Your turn human! Now, play!!!"," ");
+    if (userPlay != null){
+    console.log(userPlay);
+    }
+}
