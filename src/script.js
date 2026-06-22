2070a1920cf4f498e79063aabc931b97c86f89d7
Omar Elsharkawy <omarx10050@gmail.com>
2026-06-22 19:30:47 +0300

    Scorring done

--- src/script.js
+++ src/script.js
@@ -1,16 +1,29 @@
 import { userPlayValidate, computerPlay } from "./gamePlayInput.js";
 
-const score = 0;
-
+let computerScore = 0;
+let playerScore = 0;
+let gameFinished = false;
 function game() {
     for (let round = 1; round <= 5; round++) {
         const playerSelection = userPlayValidate();
         const computerSelection = computerPlay();
 
         const roundResult = playRound(playerSelection, computerSelection);
-
-        console.log(roundResult);
+            console.log(`Round ${round}`);
+            console.log(roundResult);
+            if(round ===5){
+                gameFinished = true;
+            }
     }
+      if(gameFinished === true){
+        if(computerScore < playerScore){
+             console.log(`You Win, Final score: you ${playerScore} -- computer ${computerScore} `);
+        }else if(computerScore > playerScore){
+             console.log(`You Lost, Final score: you ${playerScore} -- computer ${computerScore} `);
+        }else{
+             console.log(`A tie, Final score: you ${playerScore} -- computer ${computerScore} `);
+    }
+    }   
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -23,7 +36,8 @@ function playRound(playerSelection, computerSelection) {
     const computerDisplay = capitalize(computer);
 
     if (player === computer) {
-        return `It's a Tie! Both chose ${playerDisplay}`;
+        return `It's a Tie! Both chose ${playerDisplay} 
+         Your score: ${playerScore} -- Computer score: ${computerScore}`;
     }
 
     const beats = {
@@ -33,10 +47,15 @@ function playRound(playerSelection, computerSelection) {
     };
 
     if (beats[player] === computer) {
-        return `You Win! ${playerDisplay} beats ${computerDisplay}`;
+        playerScore++;
+        return `You Win! ${playerDisplay} beats ${computerDisplay}
+        Your score: ${playerScore} -- Computer score: ${computerScore}`;
     } else {
-        return `You Lose! ${computerDisplay} beats ${playerDisplay}`;
+        computerScore++;
+        return `You Lose! ${computerDisplay} beats ${playerDisplay} 
+        Your score: ${playerScore} -- Computer score: ${computerScore}`;
     }
+   
 }
 
 
