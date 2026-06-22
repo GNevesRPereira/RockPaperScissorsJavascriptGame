668b20778963df4c8d93aecf8cd5eb93826a12a7
GNevesRPereira <92853292+GNevesRPereira@users.noreply.github.com>
2026-06-22 17:57:00 +0100

    Update gamePlayInput.js

--- src/gamePlayInput.js
+++ src/gamePlayInput.js
@@ -24,25 +24,31 @@ function userPlayValidate() {
 
         invalidAttempts++;
 
-        if (invalidAttempts >= 3) {
+        if (invalidAttempts >= 1) {
             alert("Human... the game is called Rock, Paper, Scissors. Focus.");
-        } else {
-            alert("Insert your play correctly: Rock, Paper or Scissors!");
+        } else if (invalidAttempts >= 2) {
+            alert("Seriously?!! Come on... Insert your play correctly: Rock, Paper or Scissors!");
+        } else if (invalidAttempts >= 3) {
+            alert("Seriously?!! Come on... Once again it's: Rock, Paper or Scissors!");
+        } else if (invalidAttempts >= 4) {
+            alert("You can't be this stupid... It's Rock, Paper or Scissors!ROCK, PAPER OR SCISSORS! YOU DOLT!!!");
+        } else if (invalidAttempts >= 5) {
+            alert("I would recommend, that you should be neutered, in order to ensure the future of your species...You can at least, write ROCK, PAPER OR SCISSORS... RIGHT?!! Go on, i believe in you");
         }
+    
+        function getRandomItem(items) {
+            const randomIndex = Math.floor(Math.random() * items.length);
+            return items[randomIndex];
+        }
+
+        function computerPlay() {
+            return getRandomItem(gameValues);
+        }
+
+        export {
+            gameValues,
+            userPlayValidate,
+            computerPlay
+        };
     }
 }
-
-function getRandomItem(items) {
-    const randomIndex = Math.floor(Math.random() * items.length);
-    return items[randomIndex];
-}
-
-function computerPlay() {
-    return getRandomItem(gameValues);
-}
-
-export {
-    gameValues,
-    userPlayValidate,
-    computerPlay
-};
