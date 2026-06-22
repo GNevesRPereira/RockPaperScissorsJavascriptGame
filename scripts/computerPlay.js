a96e7e8ce877c3a277a0916e302a5dc5fdc5fdeb
GNevesRPereira <92853292+GNevesRPereira@users.noreply.github.com>
2026-06-22 11:50:28 +0100

    JS files change

--- scripts/computerPlay.js
+++
@@ -1,9 +0,0 @@
-
-const gameValues = ["Rock", "Paper", "Scissor"];
-
-function computer_play(arr){
-const randomSelect = Math.floor(Math.random() * arr.length);
-return arr[randomSelect];
-}
-    const computerRandomGameValue = computer_play(gameValues);
-    console.log(computerRandomGameValue);
---
+++ scripts/gamePlayInput.js
@@ -0,0 +1,21 @@
+
+const gameValues = ["Rock", "Paper", "Scissor"];
+
+function computer_play(arr) {
+    const randomSelect = Math.floor(Math.random() * arr.length);
+    return arr[randomSelect];
+}
+
+function userPlayValidate() {
+    let userPlay = prompt("It's your turn, human!", " ");
+
+    if (userPlay == "") {
+        console.log(userPlay);
+    } else {
+        alert("Insert your play, properly human!!!");
+    }
+
+}
+const computerRandomGameValue = computer_play(gameValues);
+console.log(computerRandomGameValue);
+
