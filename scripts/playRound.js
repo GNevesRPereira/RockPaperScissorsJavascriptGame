2567393cd4103c20594239ae69c4977781728c17
GNevesRPereira <92853292+GNevesRPereira@users.noreply.github.com>
2026-06-22 10:34:54 +0100

    Update playRound.js

--- scripts/playRound.js
+++ scripts/playRound.js
@@ -2,8 +2,17 @@
 const computerSelection = computerRandomGameValue;
 const playerSelection = "";
 
-function playRound(computerSelection, playerSelection){
+function playRound(computerSelection, playerSelection) {
 
 }
 
 console.log(computerSelection);
+
+
+
+function userPlayValidate() {
+    let userPlay = prompt("Your turn human! Now, play!!!"," ");
+    if (userPlay != null){
+    console.log(userPlay);
+    }
+}
